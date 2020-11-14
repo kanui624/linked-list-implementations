2f63fa7374d83441586e226c2ff21aa7cd039b39
Kanui <marley.kanui@gmail.com>
2020-11-13 20:26:19 -0600

    linked-list-implementations - global commit 02 - Added push method to singlyLinkedList class - Javascript implementation

--- javascript/linkedList.js
+++ javascript/linkedList.js
@@ -11,4 +11,22 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  // Create push method
+  push(val) {
+    // Instantiate a new Node
+    const newNode = new Node(val);
+    // If the list is empty, make a node head and tail
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    // Increment length by 1
+    this.length++;
+    // Return the list
+    return this;
+  }
 }
