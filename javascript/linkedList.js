e712d291ffd21b226d37cedb6b51c4ed80adebc2
Kanui <marley.kanui@gmail.com>
2020-11-13 21:22:58 -0600

    linked-list-implementations - global commit 04 - Recreated addNode method to the SinglyLinkedList class - Javascript Implementation

--- javascript/linkedList.js
+++ javascript/linkedList.js
@@ -1,50 +1,32 @@
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+  constructor(nodeData, nextNode = null) {
+    this.nodeData = nodeData;
+    this.nextNode = nextNode;
   }
 }
 
 class SinglyLinkedList {
   constructor() {
     this.head = null;
-    this.tail = null;
     this.length = 0;
   }
 
-  // Create push method
-  push(val) {
-    // Instantiate a new Node
-    const newNode = new Node(val);
-    // If the list is empty, make a node head and tail
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    // Increment length by 1
+  // Instantiate a new Node
+  // Pass the input as the nodeData argument in the Node class
+  // Pass the current head object as the nextNode
+  // Set the new Node object as the head
+  // increment the length of the list by 1
+  addNode(input) {
+    this.head = new Node(input, this.head);
     this.length++;
-    // Return the list
-    return this;
-  }
-
-  // Create an Unshift method
-  unshift(val) {
-    const newNode = new Node(val);
-    // If the list is empty
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-      // Add current head pointer to the new head(new node) to make the new node the head
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-    // Increment length by 1
-    this.length++;
-    // Return the list
-    return this;
   }
 }
+
+const singly = new SinglyLinkedList();
+
+singly.addNode(10);
+singly.addNode(1);
+// const dick = singly.insert(1);
+// const shit = singly.add(8);
+
+console.log(singly);
