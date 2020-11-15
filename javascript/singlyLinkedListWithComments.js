d0324ab91731bf6ceafc34bce999d8ed6dbb7fbd
Kanui <marley.kanui@gmail.com>
2020-11-14 18:47:12 -0600

    linked-list-implementations - global commit 12 - Copied file and removed comments - Javascript Implementation

--- javascript/singlyLinkedListWithComments.js
+++ javascript/singlyLinkedListWithComments.js
@@ -11,12 +11,27 @@ class SinglyLinkedList {
     this.nodeCount = 0;
   }
 
+  // ADDNODETOFRONT:
+  // Instantiate a new Node
+  // Pass the input as the nodeData argument in the Node class
+  // Pass the current head object as the nextNode
+  // Set the new Node object variable as the head
+  // increment the length of the list by 1
   addNodeToFront(input) {
     let newHeadNode = new Node(input, this.head);
     this.head = newHeadNode;
     this.nodeCount++;
   }
 
+  // ADDNODETOBACK:
+  // Instantiate a new Node and put it in a variable newTailNode
+  // Define a variable to represent the current head node
+  // If there isn't a head node then set it to the new instance
+  // else set the currentHead variable to the current head
+  // and while there is a nextNode in the head of the list:
+  // set the currentHead to the nextNode value
+  // When we reached the end of the list set the last nextNode value to the new Node instance
+  // then increment the length of the list
   addNodeToBack(input) {
     let newTailNode = new Node(input);
     let currentHead;
@@ -34,6 +49,21 @@ class SinglyLinkedList {
     this.nodeCount++;
   }
 
+  // ADDNODEATINDEX:
+  // Accept an input and index argument
+  // If the index is 0 pass the input to the addNodeToFront method
+  // If the index is the last index in the list pass the input to the addNodeToBack method
+  // If the index passed is anywhere inbetween the already constructed list then:
+  // Instantiate a new Node object with the input and place in a newNodeAtIndex variable
+  // Define and currentNode and previousNode variables to work with
+  // Set the currentNode to the head of the list
+  // Set a count variable initialize to 0
+  // while the count does not equal the index:
+  // traverse the list iteratively setting the currentNode as the previousNode and incrementing the count
+  // Once the count equals the index, set the nextNode of the node of the currentNode to the currentNode
+  // Finally set the nextNode key of the new instance of the Node object to the currentNode
+  // Set the nextNode key of the previousNode to the new Node instance
+  // and increment the nodeCount
   addNodeAtIndex(input, index) {
     if (index < 0 || index > this.nodeCount) {
       return;
@@ -58,6 +88,12 @@ class SinglyLinkedList {
     }
   }
 
+  // GETNODEATINDEX:
+  // If the index is out of range log a notification
+  // set currentNode and count variables to the head and 0 respectively
+  // While currentNode:
+  // if count === index log the nextNode value of the currentNode
+  // else increment the count by one and set the currentNode variable to the nextNode value of the currentNode
   getNodeAtIndex(index) {
     if (index < 0 || index > this.nodeCount) {
       console.log(`Index ${index} is out of Range`);
@@ -108,6 +144,11 @@ class SinglyLinkedList {
     console.log('Clear list was called, list data was erased');
   }
 
+  // PRINTLIST:
+  // Set the current head of the list to currentHead
+  // while currentHead has a value:
+  // console log that nodes value
+  // update the currentHead variable with the nextNode value
   printList() {
     if (this.head && this.nodeCount != 0) {
       let currentHead = this.head;
