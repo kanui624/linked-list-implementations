c5452b336bb01c70d7cc6863a93a26357d8c132e
Kanui <marley.kanui@gmail.com>
2020-11-14 19:02:25 -0600

    linked-list-implementations - global commit 14 - Added addToFront method to linkedList Prototype - Javascript Implementation

--- javascript/singlyLinkedListVTwo.js
+++ javascript/singlyLinkedListVTwo.js
@@ -12,4 +12,14 @@ class LinkedList {
   }
 }
 
+LinkedList.prototype.addToFront = function (input) {
+  let newNode = new Node(input);
+
+  newNode.nextNode = this.head;
+
+  this.head = newNode;
+
+  return this.head;
+};
+
 let list = new LinkedList();
