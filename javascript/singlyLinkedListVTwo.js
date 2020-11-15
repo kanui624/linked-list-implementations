1079102131e7e7aac44e15419872860876a644a6
Kanui <marley.kanui@gmail.com>
2020-11-14 23:27:28 -0600

    linked-list-implementations - global commit 17 - Update getIndex helper method to linkedList Prototype - Javascript Implementation

--- javascript/singlyLinkedListVTwo.js
+++ javascript/singlyLinkedListVTwo.js
@@ -22,33 +22,43 @@ LinkedList.prototype.addToFront = function (input) {
   return this.head;
 };
 
-LinkedList.prototype.addToBack = function (input) {
+LinkedList.prototype.addToEnd = function (input) {
   let newNode = new Node(input);
-
   if (!this.head) {
     this.head = newNode;
     return this.head;
-  } else {
-    let tail = this.head;
-    while (tail.nextNode !== null) {
-      tail = tail.nextNode;
-    }
-    tail.nextNode = newNode;
-    return this.head;
   }
+  let tail = this.head;
+  while (tail.nextNode !== null) {
+    tail = tail.nextNode;
+  }
+  tail.nextNode = newNode;
+  return this.head;
 };
 
-LinkedList.prototype.getAtIndex = function (index) {
+LinkedList.prototype.getIndex = function (index) {
   let count = 0;
   let currentNode = this.head;
   while (currentNode) {
     if (count === index) {
       return currentNode;
+    } else {
+      count++;
+      currentNode = currentNode.nextNode;
     }
-    count++;
-    currentNode = currentNode.nextNode;
   }
   return null;
 };
 
+LinkedList.prototype.printLinkedList = function () {
+  console.log(JSON.stringify(this.head, null, 4));
+};
+
 let list = new LinkedList();
+list.addToFront(3);
+list.addToFront(2);
+list.addToFront(1);
+list.addToEnd(4);
+list.addToEnd(4);
+list.insertAtIndex(0, 300);
+list.printLinkedList();
