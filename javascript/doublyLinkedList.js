38c367fe50852efd8bdaaa24062338cb17160db5
Kanui <marley.kanui@gmail.com>
2020-11-16 16:48:54 -0600

    linked-list-implementations - global commit 22 - created doublyLinkedList file and defined Node and doublyLinkedList classes - Javascript Implementation

--- javascript/doublyLinkedList.js
+++ javascript/doublyLinkedList.js
@@ -12,4 +12,26 @@ class doublyLinkedList {
     this.tail = null;
     this.listLength = 0;
   }
+
+  addHead(input) {
+    let newNode = new Node(input);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.head.previousNode = newNode;
+      newNode.nextNode = this.head;
+      this.head = newNode;
+      this.listLength++;
+      return this;
+    }
+  }
 }
+
+const newList = new doublyLinkedList();
+newList.addHead(3);
+newList.addHead(2);
+
+newList.addTail(4);
+
+newList.printList();
